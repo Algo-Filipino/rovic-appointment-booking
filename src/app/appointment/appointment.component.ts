84bb09f01a8eff05c0ac0b2f82fc377fd9796a05
John Rovie R. Balingbing <rvc.blngbng@gmail.com>
2023-07-02 15:45:17 +0800

    Added 15 days for selecting date in appointment

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -8,6 +8,7 @@ import { AppointmentService } from './appointment.service';
 })
 export class AppointmentComponent implements OnInit {
   appointments: any[] = [];
+  appointmentDates: any[] = [];
 
   constructor(private appointmentService: AppointmentService) { }
 
@@ -16,5 +17,38 @@ export class AppointmentComponent implements OnInit {
       .subscribe((data: any[]) => {
         this.appointments = data;
       });
+
+    this.populateAppointmentDates();
+  }
+
+  populateAppointmentDates() {
+    const today = new Date();
+    const next15Days = new Date();
+    next15Days.setDate(today.getDate() + 15);
+
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    while (today <= next15Days) {
+      const appointmentDate = {
+        month: months[today.getMonth()],
+        day: today.getDate(),
+        dayName: '',
+        isToday: false
+      };
+
+      if (today.toDateString() === new Date().toDateString()) {
+        appointmentDate.dayName = 'Today';
+        appointmentDate.isToday = true;
+      } else if (today.toDateString() === new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toDateString()) {
+        appointmentDate.dayName = 'Tomorrow';
+      } else {
+        appointmentDate.dayName = days[today.getDay()];
+      }
+
+      this.appointmentDates.push(appointmentDate);
+
+      today.setDate(today.getDate() + 1);
+    }
   }
 }
